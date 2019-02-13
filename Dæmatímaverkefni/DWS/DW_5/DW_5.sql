7b0727871f0907cee008d4163bfb9eb81cf8dc97
joningi98 <joningi17201@gmail.com>
2019-02-13 19:30:58 +0000

    GA_2

--- Dæmatímaverkefni/DWS/DW_5/DW_5.sql
+++ Dæmatímaverkefni/DWS/DW_5/DW_5.sql
@@ -1,13 +1,16 @@
 -- Dæmi 1 --
-create view People_Accounts
+create or replace view People_Accounts
 as
-  select ar.rbalance, p.pname
-  from accountrecords ar
-  inner join accounts a on ar.aid = a.aid
-  inner join people p on a.pid = p.pid
-  where p.pid = a.pid and
-        ar.aid = a.aid
-  order by a.aid, ar.rid;
+  select p.*, a2.*
+from people p
+inner join accounts a on p.pid = a.pid
+inner join accountrecords a2 on a.aid = a2.aid;
+
+SELECT *
+FROM People_Accounts
+WHERE pid = 200
+ORDER BY aid, rid;
+
 
 -- Dæmi 2 --
 create or replace FUNCTION Make_New_Account(in n_adate date,in n_abalance INT,
@@ -29,35 +32,45 @@ $$
 LANGUAGE plpgsql;
 
 
-SELECT make_new_account('2018-02-11',333,444, 'Jón', 'M', 1.75);
-
-SELECT *
-FROM accounts a
-WHERE a.adate = '2018-02-11';
-
-
 -- Dæmi 3 --
 CREATE OR REPLACE FUNCTION Check_people_insert()
 RETURNS TRIGGER
   AS $$
   BEGIN
-    IF people.pgender = 'M' or people.pgender = 'F' or people.pheight > 0 THEN
-      INSERT INTO people(pname, pgender, pheight)
-      VALUES(pname, pgender, pheight);
+    IF (NEW.pgender != 'M' or NEW.pgender != 'F') THEN
+      RAISE EXCEPTION 'Wrong input'
+        USING ERRCODE = '45000';
     END IF;
-
-    RETURN;
+    IF (NEW.pheight < 0) THEN
+      RAISE EXCEPTION 'Height cannot be negative'
+      USING ERRCODE = '45000';
+    END IF;
+      RETURN NEW;
 END $$
 LANGUAGE plpgsql;
 
 
-CREATE TRIGGER peron_insert_check BEFORE INSERT
+CREATE TRIGGER peron_insert_check AFTER INSERT
   ON people
 FOR EACH ROW
-EXECUTE PROCEDURE Check_people_insert();
+  EXECUTE PROCEDURE Check_people_insert();
 
 
-
-
-
-
+-- Dæmi 4 --
+CREATE OR REPLACE PROCEDURE Check_accounts()
+LANGUAGE plpgsql
+AS $$
+BEGIN
+  PERFORM *
+  FROM accounts A
+  WHERE (A.abalance != 0 OR
+         EXISTS( SELECT * FROM accountrecords R1
+           WHERE A.aid = R1.aid))
+  AND NOT EXISTS( SELECT * FROM accountrecords R
+    WHERE A.aid = R.aid
+    AND A.adate = R.rdate
+    AND A.abalance = R.rbalance
+    AND R.aid = (SELECT MAX(r2.rid) FROM accountrecords r2
+    WHERE r2.aid = a.aid));
+  RETURN;
+END $$;
--- Group_Assignment/GA_2/GA_2.sql
+++ Group_Assignment/GA_2/GA_2.sql
@@ -1,8 +1,7 @@
 -- Dæmi 1 ---
-select *
+select count(*)
 from results r
 where r.result is NULL;
-
 
 -- Dæmi 2 --
 select P.id as id, P.name as name
@@ -12,7 +11,6 @@ select P.id as id, P.name as name
 from people P
 inner join results r on r.peopleid = P.id;
 
-
 -- Dæmi 3 --
 select DISTINCT p.name, s.name, c.held
 from people p, sports s
@@ -20,7 +18,6 @@ inner join results r on s.id = r.sportid
 inner join competitions c on r.competitionid = c.id
 where date_part('year', c.held) = 2002
 AND date_part('month', c.held) = 06;
-
 
 -- Dæmi 4 --
 select p.name, s.name, s.record
@@ -31,5 +28,53 @@ where s.record = r.result
 group by p.name, s.name, s.record
 having count(r.result = s.id) = 1;
 
-
 -- Dæmi 5 --
+SELECT DISTINCT s.id, s.name, p.name, to_char(max(r.result), 'FM99.99') as maxres
+from sports s, people p
+inner join results r on p.id = r.peopleid
+inner join competitions c on r.competitionid = c.id
+group by s.id, s.name, p.name, c.held
+HAVING max(r.result) = s.record;
+
+-- Dæmi 6 --
+SELECT p.id, p.name, count(r.result = s.record)
+FROM results r
+INNER JOIN people p ON r.peopleid = p.id
+INNER JOIN sports s ON r.result = s.record
+GROUP BY p.id
+HAVING COUNT(DISTINCT(r.result))>=2;
+
+-- Dæmi 7 --
+SELECT p.id, p.name, p.height, s.name, r.result, s.record,
+       CASE WHEN r.result = s.record THEN 'Yes'
+            ELSE 'No'
+        END AS Record
+FROM people p
+JOIN results r on p.id = r.peopleid
+JOIN sports s on r.sportid = s.id;
+
+-- Dæmi 8 --
+SELECT p.name
+FROM people p
+INNER JOIN results r on p.id = r.peopleid
+INNER JOIN competitions c on r.competitionid = c.id
+GROUP BY p.name
+Having count(DISTINCT c.place) >= 10;
+
+-- Dæmi 9 --
+select p.name, p.id
+from people p
+inner join results r on p.id = r.peopleid
+inner join sports s on r.sportid = s.id
+group by p.name, s.id, p.id
+having count(r.sportid) = 7;
+
+
+-- Dæmi 10 --
+SELECT s.id, s.name, s.record, min(r.result)
+FROM sports s
+INNER JOIN results r on s.id = r.sportid
+INNER JOIN competitions c on r.competitionid = c.id
+GROUP BY s.id, c.place
+HAVING COUNT(r.result) >= 1;
+
